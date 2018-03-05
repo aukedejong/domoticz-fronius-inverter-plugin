3a15444c89813d6e83a16ff13c02a4660298f0d7
Auke de Jong <aukedejong@gmail.com>
2018-03-05 10:16:49 +0100

    First public version.

--- README.md
+++ README.md
@@ -1,7 +1,64 @@
 # domoticz-fronius-inverter-plugin
 Domoticz Fronius Inverter plugin
+--------------------------------
 
-This plugin is not yet ready for use!
-When I think it is, I'll let you know in this readme.
+This version of the plugin is running on my Domoticz 3.8799 sinds 2 feb 2018 now.
+It's not feature complete, but it's working, so I decided to share.
+
+It creates 2 devices on the Utility page.
+One custom meter showing only the current generated Watts.
+The other is a kWh type meter
+
+The Fronius API only returns the generated kWhs as an integer.
+To make the graphs more fluent the plugin calculates the fractions by using the current Watts generated,
+until the intverter return one kWh more. Then the fraction part is reset and starts calculating again.
+
+Comparing the Fronius phone app with this plugin, there is a small difference. Acceptable for me.
+
+- Fronius app - Dommoticz plugin (kWh generated per day)
+- 10.4 - 10.692
+- 8.62 - 8.431
+- 5.72 - 5.859
+
+Installation
+------------
+
+In your `domoticz/plugins` directory do
+
+```bash
+git clone https://github.com/aukedejong/domoticz-fronius-inverter-plugin.git
+```
+
+Restart your Domoticz service with:
+
+```bash
+sudo service domoticz.sh restart
+```
+
+Now go to **Setup**, **Hardware** in Domoticz. There you add
+**Fronius Inverter**.
+
+Fill in the IP address and device ID.
+For me the device ID is 1.
+
+Currently the plugin only supports Fronius API version 1
+
+Feature to add
+--------------
+
+- Debug options.
+- Option to select which devices should be created
+- Option the disable the fraction calculation feature
+- Detection of Fronius API version
+- Some things I can't remember now.
 
 I don't have much spare time, so this can take a while.
+
+
+This plugin uses an icon by 
+<a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">
+Vectors Market</a> from 
+<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+and is licensed by 
+<a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">
+CC 3.0 BY</a>
---
+++ plugin.py
@@ -0,0 +1,196 @@
+#           Fronius Inverter Plugin
+#
+#           Author:     ADJ, 2018
+#
+"""
+<plugin key="froniusInverter" name="Fronius Inverter" author="ADJ" version="0.0.1" wikilink="https://github.com/aukedejong/domoticz-fronius-inverter-plugin.git" externallink="http://www.fronius.com">
+    <params>
+        <param field="Mode1" label="IP Address" required="true" width="200px" />
+        <param field="Mode2" label="Device ID" required="true" width="100px" />
+        <param field="Mode6" label="Debug" width="100px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal" default="true" />
+                <option label="Logging" value="File"/>
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
+import Domoticz
+import sys
+import json
+import datetime
+import urllib.request
+import urllib.error
+
+class BasePlugin:
+    inverterWorking = True
+    intervalCounter = None
+    previousTotalWh = 0
+    previousCurrentWatt = 0
+    whFraction = 0
+
+    def onStart(self):
+        if Parameters["Mode6"] != "Normal":
+            Domoticz.Debugging(1)
+
+        if (len(Devices) == 0):
+            Domoticz.Device(Name="Current power",  Unit=1, TypeName="Custom", Options = { "Custom" : "1;Watt"}, Used=1).Create()
+            Domoticz.Device(Name="Total power",  Unit=2, TypeName="kWh", Used=1).Create()
+            logDebugMessage("Devices created.")
+
+        Domoticz.Heartbeat(30)
+        self.intervalCounter = 0
+
+        if ('FroniusInverter' not in Images): Domoticz.Image('Fronius Inverter Icons.zip').Create()
+        if ('FroniusInverterOff' not in Images): Domoticz.Image('Fronius Inverter Off Icons.zip').Create()
+
+        Devices[1].Update(0, sValue=str(Devices[1].sValue), Image=Images["FroniusInverter"].ID)
+        Devices[2].Update(0, sValue=str(Devices[2].sValue), Image=Images["FroniusInverter"].ID)
+        return True
+
+
+    def onHeartbeat(self):
+
+        if self.intervalCounter == 1:
+
+            ipAddress = Parameters["Mode1"]
+            deviceId = Parameters["Mode2"]
+            jsonObject = self.getInverterRealtimeData( ipAddress, deviceId )
+
+            if (self.isInverterActive(jsonObject)):
+
+                self.updateDeviceCurrent(jsonObject)
+                self.updateDeviceMeter(jsonObject)
+
+                if (self.inverterWorking == False):
+                    self.inverterWorking = True
+
+            else:
+                self.logErrorCode(jsonObject)
+
+                if (self.inverterWorking == True):
+                    self.inverterWorking = False
+                    self.updateDeviceOff()
+
+
+            self.intervalCounter = 0
+
+        else:
+            self.intervalCounter = 1
+            #logDebugMessage("Do nothing: " + str(self.intervalCounter))
+
+
+        return True
+
+
+    def getInverterRealtimeData(self, ipAddress, deviceId):
+
+        url = "http://" + ipAddress + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceID=" + deviceId + "&DataCollection=CommonInverterData"
+        logDebugMessage('Retrieve solar data from ' + url)
+
+        try:
+            req = urllib.request.Request(url)
+            jsonData = urllib.request.urlopen(req).read()
+            jsonObject = json.loads(jsonData.decode('utf-8'))
+        except (urllib.error.HTTPError, urllib.error.URLError) as e:
+            logErrorMessage("Error: " + str(e) + " URL: " + url)
+            return
+
+        #logDebugMessage("JSON: " + str(jsonData))
+
+        return jsonObject
+
+
+
+    def isInverterActive(self, jsonObject):
+
+        return jsonObject["Head"]["Status"]["Code"] == 0
+
+
+    def logErrorCode(self, jsonObject):
+
+        code = jsonObject["Head"]["Status"]["Code"]
+        reason = jsonObject["Head"]["Status"]["Reason"]
+        if (code != 12):
+            logErrorMessage("Code: " + str(code) + ", reason: " + reason)
+
+        return
+
+
+    def updateDeviceCurrent(self, jsonObject):
+
+        currentWatts = jsonObject["Body"]["Data"]["PAC"]["Value"]
+
+        Devices[1].Update(currentWatts, str(currentWatts), Images["FroniusInverter"].ID)
+
+        return
+
+    def updateDeviceMeter(self, jsonObject):
+        totalWh = jsonObject["Body"]["Data"]["TOTAL_ENERGY"]["Value"]
+        currentWatts = jsonObject["Body"]["Data"]["PAC"]["Value"]
+
+        if (self.previousTotalWh < totalWh):
+            logDebugMessage("New total recieved: prev:" + str(self.previousTotalWh) + " - new:" + str(totalWh) + " - last faction: " + str(self.whFraction))
+            self.whFraction = 0
+            self.previousTotalWh = totalWh
+
+        else:
+            averageWatts =  (self.previousCurrentWatt + currentWatts) / 2
+            self.whFraction = self.whFraction + int(round(averageWatts / 60))
+            logDebugMessage("Fraction calculated: " + str(currentWatts) + " - " + str(self.whFraction))
+
+
+        self.previousCurrentWatt = currentWatts
+        calculatedWh = totalWh + self.whFraction
+        Devices[2].Update(0, str(currentWatts) + ";" + str(calculatedWh))
+
+        return
+
+
+    def updateDeviceOff(self):
+
+        Devices[1].Update(0, "0", Images["FroniusInverterOff"].ID)
+        calculatedWh = self.previousTotalWh + self.whFraction
+        Devices[2].Update(0, "0;" + str(calculatedWh))
+
+
+    def onStop(self):
+        logDebugMessage("onStop called")
+        return True
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+    global _plugin
+    _plugin.onStart()
+
+def onStop():
+    global _plugin
+    _plugin.onStop()
+
+def onDisconnect(Connection):
+    global _plugin
+    _plugin.onDisconnect(Connection)
+
+def onHeartbeat():
+    global _plugin
+    _plugin.onHeartbeat()
+
+def logDebugMessage(message):
+    if (Parameters["Mode6"] == "Debug"):
+        now = datetime.datetime.now()
+        f = open(Parameters["HomeFolder"] + "fronius-inverter-plugin.log", "a")
+        f.write("DEBUG - " + now.isoformat() + " - " + message + "\r\n")
+        f.close()
+    Domoticz.Debug(message)
+
+def logErrorMessage(message):
+    if (Parameters["Mode6"] == "Debug"):
+        now = datetime.datetime.now()
+        f = open(Parameters["HomeFolder"] + "fronius-inverter-plugin.log", "a")
+        f.write("ERROR - " + now.isoformat() + " - " + message + "\r\n")
+        f.close()
+    Domoticz.Error(message)
